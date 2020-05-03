91a06a4a7d18ddb99d8288835c63016853a4bfd7
mlipkb2eqfwq <trey.causey@outlook.com>
2020-05-03 08:55:47 -0400

    small changes...abudhabi not showing up, testing again

---
+++ abudhabi.php
@@ -0,0 +1,216 @@
+<?php
+
+print <<<text_limiter
+
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/modernizr.js"></script>
+	<link rel="icon" sizes="600x600" href="ff_white1.png">
+	<script src="js/jquery-2.1.1.js"></script>
+	<script src="js/jquery.menu-aim.js"></script>
+	<script src="js/main.js"></script>
+  	
+	<title>Formula Fantasy 2020</title>
+</head>
+<body>
+	<header>
+		<img src="ff_white1.png" alt="Italian Trulli" align="right" height="100%" width="40%">
+		<div class="cd-dropdown-wrapper">
+			<a class="cd-dropdown-trigger" href="#0">Menu</a>
+			<nav class="cd-dropdown">
+				<h2>Menu</h2>
+				<a href="#0" class="cd-close">Close</a>
+				<ul class="cd-dropdown-content">
+					<li>
+						<form class="cd-search">
+							<input type="search" placeholder="Search...">
+						</form>
+					</li>
+						
+							<li><a href="index.php">Home</a></li>
+							<li><a href="anal.php">Analytics</a></li>
+							<li><a href="rules.php">Rules</a></li>
+							<li><a href="draft picks.php">Draft Picks</a></li>
+							<li><a href="trades.php">Trades</a></li>
+							<li><a href="f1-start.php">Reaction Time</a></li>
+							
+						<!-- fuck this thing. try again later	<li class="cd-divider">	<link rel="icon" sizes="600x600" href="ff_white1.png"></li> -->
+												
+					<li class="has-children">
+						<a href=" ">Races</a>
+
+						<ul class="cd-secondary-dropdown is-hidden">
+							<li class="go-back"><a href="#0">Main Menu</a></li>
+
+							<li class="has-children">
+								<a href=" ">Race Standings</a>
+
+
+								<ul class="is-hidden">
+									<li class="go-back"><a href="#0">2020</a></li>
+									<li><a href="australia.php">Australia</a></li>
+									<li><a href="bahrain.php">Bahrain</a></li>
+									<li><a href="vietnam.php">Vietnam</a></li>
+									<li><a href="netherlands.php">Netherlands</a></li>
+									<li><a href="spain.php">Spain</a></li>
+									<li><a href="monaco.php">Monaco</a></li>
+									<li><a href="baku.php">Azerbaijan</a></li>
+									<li><a href="canada.php">Canada</a></li>
+									<li><a href="france.php">France</a></li>
+									<li><a href="austria.php">Austria</a></li>								
+									<li><a href="great britain.php">Great Britain</a></li>
+									<li><a href="hungary.php">Hungary</a></li>
+									<li><a href="belgium.php">Belgium</a></li>
+									<li><a href="italy.php">Italy</a></li>
+									<li><a href="singapore.php">Singapore</a></li>
+									<li><a href="russia.php">Russia</a></li>
+									<li><a href="japan.php">Japan</a></li>
+									<li><a href="usa.php">United States</a></li>
+									<li><a href="mexico.php">Mexico</a></li>
+									<li><a href="brazil.php">Brazil</a></li>
+									<li><a href="abudhabi.php">Abu Dhabi</a></li>
+
+										<ul class="is-hidden">
+											<li class="go-back"><a href="#0"></a></li>
+											<li><a href=" ">Plain</a></li>
+											<li><a href=" ">Print</a></li>
+											<li><a href=" ">Striped</a></li>
+											<li><a href=" ">Long sleeved</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+						</ul> <!-- .cd-secondary-dropdown -->
+					</li> <!-- .has-children -->
+
+					<li class="has-children">
+						<a href=" ">Past Seasons</a>
+						<ul class="cd-secondary-dropdown is-hidden">
+							<li class="go-back"><a href="#0">Menu</a></li>
+							<li class="has-children">
+								<a href=" ">2019</a>
+
+							<ul class="is-hidden">
+									<li class="go-back"><a href="#0">2019</a></li>
+									<li class="has-children">
+										<ul class="is-hidden">
+											<li class="go-back"><a href="#0">2019 Season</a></li>
+											<li><a href=" ">2019 Season Points</a></li>
+										</ul>
+									</li>
+										<ul class="is-hidden">
+											<li class="go-back"><a href="#0">Past Season</a></li>
+										</ul>
+									</li>
+								<li><a href="2019.php">Season Total</a></li>
+							</ul>
+					</li>
+
+							<li class="has-children">
+								<a href=" ">2018</a>
+
+							<ul class="is-hidden">
+									<li class="go-back"><a href="#0">2019</a></li>
+									<li class="has-children">
+										<ul class="is-hidden">
+											<li class="go-back"><a href="#0">2018 Season</a></li>
+											<li><a href=" ">20 Season Points</a></li>
+										</ul>
+									</li>
+										<ul class="is-hidden">
+											<li class="go-back"><a href="#0">Past Season</a></li>
+										</ul>
+									</li>
+								<li><a href="2018.php">Season Total</a></li>
+							</ul>
+								</ul>
+							</li>
+						</ul> <!-- .cd-secondary-dropdown -->
+					</li> <!-- .has-children -->
+				</ul> <!-- .cd-dropdown-content -->
+			</nav> <!-- .cd-dropdown -->
+		</div> <!-- .cd-dropdown-wrapper -->
+	</header>
+
+
+
+
+
+
+
+
+		<div>
+
+			<p align="center">
+				<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTEt0pW21f1Hn0WZospcFOIPbZQZCWKJ9IMtgxVmfoxvlXbqlbBcSdrflKfG8a66XBM4_8lJvjhOifA/pubhtml?gid=1349265672&amp;single=true&amp;widget=true&amp;headers=false" width="100%" height="275" frameborder="0" scrolling="yes" padding="0" align="middle"></iframe>
+			</p>
+			<p align="center">
+				<a href="https://www.statsf1.com/en/circuit-yas-marina.aspx" target="_blank">
+					<img src="https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Circuit%20maps%2016x9/Abu_Dhabi_Circuit.png.transform/7col/image.png">
+				</a>	
+			</p>
+
+		</div>	
+
+
+		
+<!-- style / buttons -->
+
+<style>
+a {
+  text-decoration: none;
+  display: inline-block;
+  padding: 8px 16px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.previous {
+  background-color: #8d0000;
+  color: black;
+}
+
+.next {
+  background-color: #FC0404;
+  color: white; 
+}
+
+.round {
+  border-radius: 50%;
+}
+</style>
+<br>
+<center>		
+<a href="brazil.php" class="previous round">&#8249;</a>
+<a href="index.php" class="next round">&#8250;</a>
+</center>	
+
+		
+
+
+		
+
+
+		 
+
+
+		
+
+
+</table>
+
+
+</html>
+
+
+
+
+text_limiter;
+
+?>
