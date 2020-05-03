774bb653c2ea7d17bf64ba2275aa04fc5177852b
mlipkb2eqfwq <trey.causey@outlook.com>
2020-05-03 08:33:40 -0400

    added 2018.php, fixed links

---
+++ 2018.php
@@ -0,0 +1,217 @@
+<?php
+
+print <<<text_limeter
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css"> 
+	<script src="js/modernizr.js"></script> 
+	<link rel="icon" sizes="600x600" href="ff_white1.png">
+	<script src="js/jquery-2.1.1.js"></script>
+	<script src="js/jquery.menu-aim.js"></script> 
+	<script src="js/main.js"></script> 
+  	
+	<title>Formula Fantasy 2020</title>
+</head>
+<body>
+	<header>
+		<img src="ff_white1.png" alt="Italian Trulli" align="right" height="100%" width="40%">
+		<div class="cd-dropdown-wrapper">
+			<a class="cd-dropdown-trigger" href="#0">Menu</a>
+			<nav class="cd-dropdown">
+				<h2>Menu</h2>
+				<a href="#0" class="cd-close">Close</a>
+				<ul class="cd-dropdown-content">
+					<li>
+						<form class="cd-search">
+							<input type="search" placeholder="Search...">
+						</form>
+					</li>
+						
+							<li><a href="index.php">Home</a></li>
+							<li><a href="anal.html">Analytics</a></li>
+							<li><a href="rules.html">Rules</a></li>
+							<li><a href="draft picks.html">Draft Picks</a></li>
+							<li><a href="trades.html">Trades</a></li>
+							<li><a href="f1-start.html">Reaction Time</a></li>
+							
+						<!-- fuck this thing. try again later	<li class="cd-divider">	<link rel="icon" sizes="600x600" href="ff_white1.png"></li> -->
+												
+					<li class="has-children">
+						<a href=" ">Races</a>
+
+						<ul class="cd-secondary-dropdown is-hidden">
+							<li class="go-back"><a href="#0">Main Menu</a></li>
+
+							<li class="has-children">
+								<a href=" ">Race Standings</a>
+
+
+								<ul class="is-hidden">
+									<li class="go-back"><a href="#0">2020</a></li>
+									<li><a href="australia.html">Australia</a></li>
+									<li><a href="bahrain.html">Bahrain</a></li>
+									<li><a href="vietnam.html">Vietnam</a></li>
+									<li><a href="netherlands.html">Netherlands</a></li>
+									<li><a href="spain.html">Spain</a></li>
+									<li><a href="monaco.html">Monaco</a></li>
+									<li><a href="baku.html">Azerbaijan</a></li>
+									<li><a href="canada.html">Canada</a></li>
+									<li><a href="france.html">France</a></li>
+									<li><a href="austria.html">Austria</a></li>								
+									<li><a href="great britain.html">Great Britain</a></li>
+									<li><a href="hungary.html">Hungary</a></li>
+									<li><a href="belgium.html">Belgium</a></li>
+									<li><a href="italy.html">Italy</a></li>
+									<li><a href="singapore.html">Singapore</a></li>
+									<li><a href="russia.html">Russia</a></li>
+									<li><a href="japan.html">Japan</a></li>
+									<li><a href="usa.html">United States</a></li>
+									<li><a href="mexico.html">Mexico</a></li>
+									<li><a href="brazil.html">Brazil</a></li>
+									<li><a href="abudhabi.html">Abu Dhabi</a></li>
+
+										<ul class="is-hidden">
+											<li class="go-back"><a href="#0"></a></li>
+											<li><a href=" ">Plain</a></li>
+											<li><a href=" ">Print</a></li>
+											<li><a href=" ">Striped</a></li>
+											<li><a href=" ">Long sleeved</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+						</ul> <!-- .cd-secondary-dropdown -->
+					</li> <!-- .has-children -->
+
+					<li class="has-children">
+						<a href=" ">Past Seasons</a>
+						<ul class="cd-secondary-dropdown is-hidden">
+							<li class="go-back"><a href="#0">Menu</a></li>
+							<li class="has-children">
+								<a href=" ">2019</a>
+
+							<ul class="is-hidden">
+									<li class="go-back"><a href="#0">2019</a></li>
+									<li class="has-children">
+										<ul class="is-hidden">
+											<li class="go-back"><a href="#0">2019 Season</a></li>
+											<li><a href=" ">2019 Season Points</a></li>
+										</ul>
+									</li>
+										<ul class="is-hidden">
+											<li class="go-back"><a href="#0">Past Season</a></li>
+										</ul>
+									</li>
+								<li><a href="2019.html">Season Total</a></li>
+							</ul>
+					</li>
+
+							<li class="has-children">
+								<a href=" ">2018</a>
+
+							<ul class="is-hidden">
+									<li class="go-back"><a href="#0">2019</a></li>
+									<li class="has-children">
+										<ul class="is-hidden">
+											<li class="go-back"><a href="#0">2018 Season</a></li>
+											<li><a href=" ">20 Season Points</a></li>
+										</ul>
+									</li>
+										<ul class="is-hidden">
+											<li class="go-back"><a href="#0">Past Season</a></li>
+										</ul>
+									</li>
+								<li><a href="2018.html">Season Total</a></li>
+							</ul>
+								</ul>
+							</li>
+						</ul> <!-- .cd-secondary-dropdown -->
+					</li> <!-- .has-children -->
+				</ul> <!-- .cd-dropdown-content -->
+			</nav> <!-- .cd-dropdown -->
+		</div> <!-- .cd-dropdown-wrapper -->
+	</header>
+
+
+		
+
+
+<style>
+#results {
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#results td, #results th {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: center;
+}
+
+#results tr:nth-child(even){background-color: #f2f2f2;}
+
+#results tr:hover {background-color: #ddd;}
+
+#results th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: center;
+  background-color: #29a4ff;
+  color: white;
+}
+</style>
+<br>
+<table id="results">
+	<tr>
+		<th>2018 RESULTS</th>
+	</tr>
+</table>
+<BR>
+<table id="results">
+	<tr>
+		<th>Position</th>
+		<th>Player</th>
+		<th>Total Points</th>
+	</tr>
+	<tr>
+		<td>1ST</td>
+		<td>Nick</td>
+		<td>3738</td>
+	</tr>
+	<tr>
+		<td>2ND</td>
+		<td>Olivier</td>
+		<td>1585</td>
+	</tr>
+	<tr>
+		<td>3RD</td>
+		<td>Phil</td>
+		<td>1584</td>
+	</tr>
+	<tr>
+		<td>4TH</td>
+		<td>Casey</td>
+		<td>1471</td>
+	</tr>
+	<tr>
+		<td>5TH</td>
+		<td>Adam</td>
+		<td>328</td>
+	</tr>
+	<tr>
+		<td>6TH</td>
+		<td>Andrew</td>
+		<td>0</td>
+	</tr>
+</table>
+
+</html>
+text_limeter;
+
+?>
